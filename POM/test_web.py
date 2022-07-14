4da8b18b983df652a95e4d334be38297fb9f7646
brianaparchment <34357764+brianaparchment@users.noreply.github.com>
2022-07-14 14:43:18 -0400

    add locators

--- POM/test_web.py
+++ POM/test_web.py
@@ -7,3 +7,10 @@ class Pg_Locators(object):
     #quiz page locators
     page_link_txt = "Stress Quiz"
     calc_btn_xpath = "/html/body/div/div[2]/div[2]/div[2]/div[1]/button"
+
+    #id locator for each question in the quiz
+    q1 = "input_q1"
+    q2 = "input_q2"
+    q3 = "input_q3"
+    q4 = "input_q4"
+    q5 = "input_q5"
