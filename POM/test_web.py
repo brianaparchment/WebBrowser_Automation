dbf86b8e00086eae27880c01b8336269c28245fe
brianaparchment <34357764+brianaparchment@users.noreply.github.com>
2022-07-14 14:47:26 -0400

    switch contents in files

--- POM/test_web.py
+++ POM/test_web.py
@@ -1,16 +1,10 @@
-#the goal is to test the quiz function on the LessLikely website
-#step 1: open https://www.albany.edu/~BP754362/tips.html
-#step 2: locate the quiz page and complete the quiz
-#step 3: check if quiz results are displayed
+#this page contains the webpage test that checks the webpage url to ensure that the url changes to the quiz page
 
-class Pg_Locators(object):
-    #quiz page locators
-    page_link_txt = "Stress Quiz"
-    calc_btn_xpath = "/html/body/div/div[2]/div[2]/div[2]/div[1]/button"
+import random
+import time
+import unittest
 
-    #id locator for each question in the quiz
-    q1 = "input_q1"
-    q2 = "input_q2"
-    q3 = "input_q3"
-    q4 = "input_q4"
-    q5 = "input_q5"
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from web_locators import Pg_locators
+
--- web_locators.py
+++ web_locators.py
@@ -1,1 +1,16 @@
-#locators file
+#the goal is to test the quiz function on the LessLikely website
+#step 1: open https://www.albany.edu/~BP754362/tips.html
+#step 2: locate the quiz page and complete the quiz
+#step 3: check if the url is the correct url
+
+class Pg_Locators(object):
+    #quiz page locators
+    page_link_txt = "Stress Quiz"
+    calc_btn_xpath = "/html/body/div/div[2]/div[2]/div[2]/div[1]/button"
+
+    #id locator for each question in the quiz
+    q1 = "input_q1"
+    q2 = "input_q2"
+    q3 = "input_q3"
+    q4 = "input_q4"
+    q5 = "input_q5"
