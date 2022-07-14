c9ad739d8ee85452e142f72f0644ff59e61981bc
brianaparchment <34357764+brianaparchment@users.noreply.github.com>
2022-07-14 14:42:13 -0400

    add locators

--- POM/test_web.py
+++ POM/test_web.py
@@ -2,3 +2,8 @@
 #step 1: open https://www.albany.edu/~BP754362/tips.html
 #step 2: locate the quiz page and complete the quiz
 #step 3: check if quiz results are displayed
+
+class Pg_Locators(object):
+    #quiz page locators
+    page_link_txt = "Stress Quiz"
+    calc_btn_xpath = "/html/body/div/div[2]/div[2]/div[2]/div[1]/button"
