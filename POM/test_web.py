03364f110ee2a61aabf715ff7b727a0f89b6827d
brianaparchment <34357764+brianaparchment@users.noreply.github.com>
2022-07-14 14:40:52 -0400

    instructions for test

--- POM/test_web.py
+++ POM/test_web.py
@@ -1,1 +1,4 @@
-#test page
+#the goal is to test the quiz function on the LessLikely website
+#step 1: open https://www.albany.edu/~BP754362/tips.html
+#step 2: locate the quiz page and complete the quiz
+#step 3: check if quiz results are displayed
